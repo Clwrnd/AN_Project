d4dac79485b536d7560559b4880135a8287ec841
5wiip <128042740+5wiip@users.noreply.github.com>
2024-05-06 11:01:23 +0200

    Partie D question 19

--- IntermediateFiles/Partie D.py
+++ IntermediateFiles/Partie D.py
@@ -7,12 +7,7 @@ Created on Tue Apr 16 10:30:51 2024
 
 from math import *
 from matplotlib.pyplot import *
-from numpy import *
-A = 0
-
-eps = 10**(5)
-
-b = Ax
+import numpy as np
 
 def G(y,A,b) :
     G = 2 * (A * y - b)
@@ -23,15 +18,20 @@ def rhok(y,A,b) :
         rhok = 0 
         return rhok
     else : 
-        rhok = absolute(G(y,A,b)**2)/(2 * G(y,A,b) * np.transpose(A) * G(y,A,b))
+        rhok = abs(G(y,A,b)**2)/(2 * G(y,A,b) * np.transpose(A) * G(y,A,b))
         return rhok
     
+# a correspond à la première valeur testée
+# b et A correspondent bien aux valeurs de la fonction.
 
-def gradPO(eps, MaxIter, a, b): 
-    yk = 
-    while (N>MaxIter or absolute(ykplus1-yk)<eps) :
-        ykplus1 = yk - rhok * G(yk)
-        yk = yplus1
-        N= N + 1
+def gradPO(MaxIter, a, A, b): 
+    N = 0
+    yk = a
+    ykplus1 = 0
+    while (N<MaxIter or abs(ykplus1-yk)>10**(-5)) :
+        ykplus1 = yk - rhok(yk,A,b) * G(yk,A,b)
+        yk = ykplus1
+        N = N + 1
+        if (G(yk,A,b)==0):
+            N = MaxIter+1
     return yk
-        
